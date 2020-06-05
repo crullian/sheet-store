a03efbdad02bbdec85b0ae1ff208b9deffee68fe
crullian <crullian@gmail.com>
2020-06-05 10:51:25 +0100

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { database } from './firebase.js';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [members, setMembers] = useState([]);
+  useEffect(() => {
+    const itemsRef = database.ref('/');
+    itemsRef.on('value', snapshot => {
+      let members = Object.values(snapshot.val())[0].members;
+      console.log('snapshot values, son', members)
+      setMembers(members)
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        {
+          members.map(member => {
+            return (
+              <div key={member.id}>
+                {member.name} is {member.isCool ? 'cool' : 'not cool'}
+              </div>
+            )
+          })
+        }
       </header>
     </div>
   );
